f70745a0eebd167048d801b8e96a1563e4a34c5e
th0rin-idm <127588431+th0rin-idm@users.noreply.github.com>
2023-03-26 13:30:12 -0600

    Segundo commit
    Nave tomada de una página de imagenes sin copyright, el sprite se mueve en el eje y únicamente en las coordenadas de la pantalla

--- sdl2_demo/windowtest2.cpp
+++ sdl2_demo/windowtest2.cpp
@@ -1,61 +1,51 @@
 #include <SDL2/SDL.h>
-#include <iostream>
+#include <SDL2/SDL_image.h>
 
-int main(int argc, char* argv[])
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
-    // Initialize SDL2
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    // Cargar imagen
+    SDL_Surface* surface = IMG_Load("/home/vboxuser/projects/sdl2_demo/image.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    int x = 0, y = 50;
+    bool quit = false;
+    SDL_Event event;
+    while (!quit)
     {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    bool program_is_running = true;
-    
-    while (program_is_running) {
-
-        std::cout << "Program is running..." << std::endl;
-        // Lógica del programa aquí
-    
-
-        // Create a window
-        SDL_Window* window = SDL_CreateWindow(
-            "osea si3",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            640,
-            480,
-            SDL_WINDOW_SHOWN
-        );
-
-        if (window == nullptr)
-        {
-            std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
-            return 1;
-        }
-
-        // Wait for user to close window
-        SDL_Event event;
-        while (SDL_WaitEvent(&event))
+        while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
             {
-                program_is_running= false;
-                break;
+                quit = true;
+            }
+            else if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_UP && y>0) {
+                    y -= 10; // Mover la imagen 10 unidades hacia arriba
+                }  
+                else if (event.key.keysym.sym == SDLK_DOWN && y<380) {
+                    y += 10; // Mover la imagen 10 unidades hacia arriba
+                }   
             }
         }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+        SDL_RenderClear(renderer);
+        SDL_Rect rect = { x, y, 100, 100 };
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+        SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
